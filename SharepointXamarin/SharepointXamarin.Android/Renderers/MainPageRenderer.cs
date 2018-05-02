3f91e5f4e6a718c70c8f78df2d28b282bf73a2e0
Elbrinner <elbrinner@elbrinner.com>
2018-05-02 02:22:15 +0200

    Version android - necesita chorme

---
+++ SharepointXamarin/SharepointXamarin.Android/Renderers/MainPageRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SharepointXamarin.Droid.Renderers;
+using SharepointXamarin.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(LoginPage), typeof(MainPageRenderer))]
+namespace SharepointXamarin.Droid.Renderers
+{
+    class MainPageRenderer : PageRenderer
+    {
+        public MainPageRenderer(Context context) : base(context)
+        {
+
+        }
+        LoginPage page;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+        {
+            base.OnElementChanged(e);
+            page = e.NewElement as LoginPage;
+            var activity = this.Context as Activity;
+        }
+
+    }
+}
