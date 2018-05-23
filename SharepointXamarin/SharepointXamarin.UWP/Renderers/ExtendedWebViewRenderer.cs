679d397b252ecf53d6f1a430599ec49be2d61735
Elbrinner da silva fernandes <edasilvf@everis.com>
2018-05-23 13:21:21 +0200

    rename

--- SharepointXamarin/SharepointXamarin.UWP/Renderers/ExtendedWebViewRenderer.cs
+++ SharepointXamarin/SharepointXamarin.UWP/Renderers/ExtendedWebViewRenderer.cs
@@ -2,6 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.UWP;
 using SharepointXamarin.Controls;
+using SharepointXamarin.UWP.Renderers;
+
+[assembly: ExportRenderer(typeof(ExtendedWebView), typeof(ExtendedWebViewRenderer))]
 namespace SharepointXamarin.UWP.Renderers
 {
     public class ExtendedWebViewRenderer : WebViewRenderer
